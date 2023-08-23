cc151064cdb5f5cb0d96d9b3a5167f09538b17cc
dithiane <elenamena.colorado@gmail.com>
2023-08-23 14:46:16 -0600

    Created The BoilerPlate Code

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Whatsapp2</h1>
+    <main className="flex h-screen overflow-hidden">
+      <div className="w-1/3 overflow-y-auto bg-white-500">chatsidebar</div>
+      <div className="w-full overflow-y-auto flex items-center justify-center bg-blue-50">chat</div>
     </main>
   )
 }
