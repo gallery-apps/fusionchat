c6a142b441a609a6a32c5503fe396b3f4cfbce21
dithiane <elenamena.colorado@gmail.com>
2023-08-24 16:13:03 -0600

    Changes some style for the conversation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex h-screen overflow-hidden">
       <div className="w-1/3 overflow-y-auto bg-white-500">chatsidebar</div>
-      <div className="w-full overflow-y-auto flex items-center justify-center bg-blue-50">chat</div>
+      <div className="w-full overflow-y-auto flex items-center justify-center bg-blue-500">chat</div>
     </main>
   )
 }
