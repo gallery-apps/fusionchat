3f1df77d363c8c812e11ed9c915ed8e60032208b
dithiane <elenamena.colorado@gmail.com>
2023-08-30 17:43:49 -0600

    Major updates for app

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,20 +1,18 @@
-import Conversation from "@/modules/Conversation";
 import Image from "next/image";
-
-export default function Home() {
+function RightSidebar() {
   return (
     <main className="flex h-screen overflow-hidden">
-      <div className="w-1/3 overflow-y-auto bg-white-500">chatsidebar</div>
       <div className="w-full overflow-y-auto relative">
         <Image
           className="bgImage"
-          src="/bg.svg"
+          src="/assets/bg.svg"
           alt="Backgound image"
           fill
           objectFit="cover"
         />
-        <Conversation />
       </div>
     </main>
   );
 }
+
+export default RightSidebar;
---
+++ constants/index.js
@@ -0,0 +1,19 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/search.svg",
+    route: "/search",
+    label: "Search",
+  },
+
+  {
+    imgURL: "/assets/user.svg",
+    route: "/user",
+    label: "User",
+  },
+];
+
