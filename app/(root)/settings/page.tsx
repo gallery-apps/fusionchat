3f286135d9a7a64c1db11092532f5b501e29aa2f
Hamza Ali <hamza.ali@perficient.com>
2023-09-11 14:07:44 -0600

    intial commit

--- app/(root)/settings/page.tsx
+++ app/(root)/settings/page.tsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-export default function page() {
-  return <div></div>;
+export default function SettingsPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <SettingsSection title="Account Information">
+        {/* Content for Account Information section */}
+      </SettingsSection>
+
+      <SettingsSection title="General Settings">
+        {/* Content for General Settings section */}
+      </SettingsSection>
+
+      <SettingsSection title="Privacy">
+        {/* Content for Privacy section */}
+      </SettingsSection>
+
+      <SettingsSection title="Notifications">
+        {/* Content for Notifications section */}
+      </SettingsSection>
+
+      <SettingsSection title="Help">
+        {/* Content for Help section */}
+      </SettingsSection>
+    </div>
+  );
+}
+
+function SettingsSection({ title, children }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 mb-4">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {children}
+      </div>
+    </div>
+  );
 }
