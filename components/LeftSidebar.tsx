a5da87c1730387944b069c8ba785a08bcad7f4c6
Mahad Ali <mahadaali3@gmail.com>
2023-10-04 12:29:45 -0600

    minor contacts and left sidebar changes

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -13,35 +13,26 @@ function LeftSidebar({ users }: { users: User[] }) {
   const path = usePathname();
 
   return (
-    <section className="leftsidebar">
-      <div className="flex flex-col h-screen w-full px-6">
-        <div className="flex flex-row w-full gap-6 px-6 h-1/5">
-          {sidebarLinks.map((link) => (
-            <Link href={link.route} key={link.label} passHref>
-              <div className={`leftsidebar_link ${path === link.route ? 'text-teal-500 font-bold' : 'text-gray-700'}`}>
-                <Image
-                  src={link.imgURL}
-                  alt={link.label}
-                  width={24}
-                  height={24}
-                />
-                <span className="ml-2">{link.label}</span>
-              </div>
-            </Link>
-          ))}
+    <section className="leftsidebar bg-black-200 p-4 h-screen">
+      <div className="flex flex-col h-1/5 justify-between">
+        <div className="flex flex-row w-full gap-6 mb-4"> {/* Added margin-bottom */}
+          {sidebarLinks.map((link) => {
+            return (
+              <Link href={link.route} key={link.label} className="flex flex-col items-center cursor-pointer">
+                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                <p className="text-teal-500 text-lg mt-1">{link.label}</p> {/* Added margin-top */}
+              </Link>
+            );
+          })}
         </div>
-        {path.includes("/contacts") ? <Contacts users={users} path={"/contacts"} /> : null}
+        {path.includes("/contacts") && <Contacts users={users} path={"/contacts"} />}
       </div>
-      <div className="mt-10 px-6">
+      <div className="mt-10">
         <SignedIn>
           <SignOutButton signOutCallback={() => router.push("/sign-in")}>
-            <div className="flex cursor-pointer">
-              <Image
-                src="/assets/logout.svg"
-                alt="logout"
-                width={24}
-                height={24}
-              />
+            <div className="flex items-center cursor-pointer">
+              <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+              <p className="text-teal-500 ml-2">Sign Out</p>
             </div>
           </SignOutButton>
         </SignedIn>
